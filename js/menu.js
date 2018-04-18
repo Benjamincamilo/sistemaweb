5aa185eed68b24e0271c16857e508b6f054b3427
Benjamincamilo <38412441+Benjamincamilo@users.noreply.github.com>
2018-04-18 10:01:23 -0500

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+var btnMenu = document.getElementById('btn-menu');
+var nav = document.getElementById('nav');
+btnMenu.addEventListener('click', function(){
+    "use strict";
+  nav.classList.toggle('mostrar');
+});
